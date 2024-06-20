71786c157bde207b693cc1453850a6c7cbceffba
BiagioG95 <biagiograssia95@gmail.com>
2024-06-20 12:16:31 +0200

    Update Main.java

--- classiAstratte/src/Main.java
+++ classiAstratte/src/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
 
         Rettangolo rettangolo = new Rettangolo(3,10);
-        rettangolo.calcolaArea();
         System.out.println(rettangolo.calcolaArea());
 
 
