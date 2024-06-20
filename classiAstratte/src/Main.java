1eec83f0f0ff7f65adeb0fef6f36d56db44d4a32
BiagioG95 <biagiograssia95@gmail.com>
2024-06-20 11:58:35 +0200

    Add files via upload

---
+++ classiAstratte/src/Main.java
@@ -0,0 +1,17 @@
+/*
+Scrivere un programma che contenga una classe astratta chiamata Forma ed un metodo astratto chiamato calcolaArea().
+Crea poi due sottoclassi Rettangolo e Triangolo che estendono Forma ed implementano il metodo per il calcolo dell'area in maniera specifica.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Rettangolo rettangolo = new Rettangolo(3,10);
+        rettangolo.calcolaArea();
+        //System.out.println(rettangolo.calcolaArea());
+
+
+        Triangolo triangolo = new Triangolo(4, 9);
+        System.out.println(triangolo.calcolaArea());
+
+    }
+}
---
+++ classiAstratte/src/Rettangolo.java
@@ -0,0 +1,13 @@
+public class Rettangolo extends Forma{
+    private float base;
+    private float altezza;
+
+    public  Rettangolo(float base, float altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+    @Override
+    public float calcolaArea() {
+        return base * altezza;
+    }
+}
